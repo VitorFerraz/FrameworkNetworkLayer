6fefaf36cd59dad2769352ca8bcf196be6ed6f46
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2021-07-30 07:46:44 -0300

    Initial Commit

---
+++ FrameworkNetworkLayer/FrameworkNetworkLayer.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkNetworkLayer.h
+//  FrameworkNetworkLayer
+//
+//  Created by Vitor Ferraz Varela on 30/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkNetworkLayer.
+FOUNDATION_EXPORT double FrameworkNetworkLayerVersionNumber;
+
+//! Project version string for FrameworkNetworkLayer.
+FOUNDATION_EXPORT const unsigned char FrameworkNetworkLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkNetworkLayer/PublicHeader.h>
+
+
